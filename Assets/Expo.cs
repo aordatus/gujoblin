311c44ea2df34489df182db6c8733f6426bd0bf7
智Aordatus <aordatus@gmail.com>
2023-01-10 13:56:41 +0530

    backup

---
+++ Assets/Expo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expo : MonoBehaviour {
+
+	public GameObject expo;
+
+	void Crbomb()
+	{
+		Instantiate (expo);
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		Crbomb ();
+
+		Invoke ("CreateBomb", 4f);
+
+		Destroy (expo);
+
+	}
+	
+
+}
---
+++ Assets/Goblin/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController charControl;
+    public float walkSpeed;
+	Animator anim;
+
+    void Awake()
+    {
+        charControl = GetComponent<CharacterController>();
+		anim = GetComponent<Animator>();
+    }
+
+
+
+    void Update()
+    {
+        MovePlayer();
+		Attack ();
+    }
+
+	void Attack()
+	{
+		if (Input.GetKey("f")) 
+		{
+			anim.Play ("Attack");
+		
+		}
+	}
+		
+
+
+
+    void MovePlayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+		anim.SetFloat ("walk", horiz);
+		anim.SetFloat ("walk", vert);
+        Vector3 moveDirSide = transform.right * horiz * walkSpeed;
+        Vector3 moveDirForward = transform.forward * vert * walkSpeed;
+
+        charControl.SimpleMove(moveDirSide);
+        charControl.SimpleMove(moveDirForward);
+
+    }
+}
